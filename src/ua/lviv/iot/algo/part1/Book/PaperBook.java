9ff92950435c06883e9ecb3e75138cf6935ba4b9
RomaTaRn <115878448+RomaTaRn@users.noreply.github.com>
2023-05-13 19:09:56 +0300

    Update PaperBook.java

--- src/ua/lviv/iot/algo/part1/Book/PaperBook.java
+++ src/ua/lviv/iot/algo/part1/Book/PaperBook.java
@@ -13,11 +13,6 @@ public class PaperBook extends Book {
     private int heightInMm;
 
     public PaperBook(String title, String author, String publisher, int year, String genre, int pageCount, int widthInMm, int heightInMm) {
-        setTitle(title);
-        setAuthor(author);
-        setPublisher(publisher);
-        setYear(year);
-        setGenre(genre);
         setPageCount(pageCount);
         setWidthInMm(widthInMm);
         setHeightInMm(heightInMm);
